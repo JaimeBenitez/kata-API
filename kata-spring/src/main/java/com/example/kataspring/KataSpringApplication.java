49ccf41b1d156fea981f36aacae629a268f03aed
jaime <jbenaci736@g.educaaand.es>
2023-02-17 19:31:21 +0100

    Proyecto creado completo

---
+++ kata-spring/src/main/java/com/example/kataspring/KataSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.kataspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataSpringApplication.class, args);
+	}
+
+}
---
+++ kata-spring/src/test/java/com/example/kataspring/KataSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kataspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
